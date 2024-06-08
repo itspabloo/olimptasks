7ddc324931fdc00c57d9a37b2a4726713198956c
itz_pabloo <165188127+itspabloo@users.noreply.github.com>
2024-06-08 16:10:28 +0300

    Create ближайшее большее справа.cpp

---
+++ ближайшее большее справа.cpp
@@ -0,0 +1,155 @@
+// Дан массив a из n чисел. Нужно обрабатывать запросы:
+// set(i, x) – присвоить новое значение элементу массива a[i] = x;
+// get(i, x) – найти mink:k≥i и ak≥x.
+
+#include <bits/stdc++.h>//бесполезный комментарий code by pabL0
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+#define fasttype ios_base::sync_with_stdio(false); std::cin.tie(nullptr); std::cout.tie(nullptr);
+#define pb push_back
+#define pf push_front
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define sz(a) (ll)a.size()
+#define ff first
+#define ss second
+#define MOD (ll)1e9 + 7;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld; 
+using namespace __gnu_pbds;
+using namespace std;
+const string NO = "NO\n";
+const string YES = "YES\n";
+const ll inf = 1e15;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <typename T>
+istream &operator>>(istream &in, vector<T> &a) {
+    for (auto &x : a) in >> x;
+    return in;
+}
+
+template <typename T>
+ostream &operator<<(ostream &out, vector<T> &a) {
+    for (auto &x : a) out << x <<' ';
+    return out;
+}
+
+template <typename X, typename Y>
+istream &operator>>(istream &in, pair<X, Y> &a) {
+    in >> a.ff >> a.ss;
+    return in;
+}
+
+template <typename X, typename Y>
+ostream &operator<<(ostream &out, pair<X, Y> &a) {
+    out << a.ff << ' ' << a.ss;
+    return out;
+}
+
+struct SegTree {
+    ll n;
+    vector<ll> t;
+
+    SegTree(ll _n, vector<ll> &a) {
+        n = _n;
+        t.resize(4 * n);
+        build_tree(1, 0, n - 1, a);
+    }
+
+    void build_tree(ll v, ll l, ll r, vector<ll> &a) {
+        if (l == r) {
+            t[v] = a[l];
+            return;
+        }
+        ll m = (l + r) / 2;
+        build_tree(2 * v, l, m, a);
+        build_tree(2 * v + 1, m + 1, r, a);
+        t[v] = max(t[v * 2], t[v * 2 + 1]);
+    }
+
+    ll mx(ll v, ll l, ll r, ll tl, ll tr) {
+        if (tl > r || tr < l) return -inf;
+        if (tl <= l && r <= tr) return t[v];
+        ll m = (l + r) / 2;
+        return max(mx(2 * v, l, m, tl, tr), mx(2 * v + 1, m + 1, r, tl, tr));
+    }
+
+    void upd(ll v, ll l, ll r, ll p, ll x) {
+        if (l > r) return;
+        if (l == r) {
+            t[v] = x;
+            return;
+        }
+        ll m = (l + r) / 2;
+        if (p <= m) upd(2 * v, l, m, p, x);
+        else upd(2 * v + 1, m + 1, r, p, x);
+        t[v] = max(t[v * 2], t[v * 2 + 1]);
+    }
+
+    ll mx(ll l, ll r) {
+        return mx(1, 0, n - 1, l, r);
+    }
+
+    void upd(ll p, ll x) {
+        upd(1, 0, n - 1, p, x);
+    }
+
+    ll find(ll v, ll x, ll l, ll r) {
+        if (l == r) return l;
+        ll m = (r + l) / 2;
+        if (t[v * 2] >= x) return find(v * 2, x, l, m);
+        return find(v * 2 + 1, x, m + 1, r);
+    }
+
+    ll find_n(ll v, ll x, ll left, ll l, ll r) {
+        if (r < left) return -inf;
+        if (l >= left) {
+            return (t[v] < x ? -inf : find(v, x, l, r));
+        }
+        ll m = (r + l) / 2;
+        ll tmp = find_n(2 * v, x, left, l, m);
+        if (tmp != -inf) return tmp; 
+        tmp = find_n(2 * v + 1, x, left, m + 1, r);
+        if (tmp != inf) return tmp;
+        return -inf;
+    }
+};
+
+void Solve() {
+    ll n, q;
+    cin >> n >> q;
+    vector<ll> a(n);
+    cin >> a;
+    SegTree t(n, a);
+    while (q--) {
+        ll type;
+        cin >> type;
+        if (!type) {
+            ll p, x;
+            cin >> p >> x;
+            t.upd(p - 1, x);
+        } else {
+            ll i, x;
+            cin >> i >> x;
+            if (t.mx(i - 1, n - 1) < x) {
+                cout << -1 << '\n';
+                continue;
+            }
+            ll ans = t.find_n(1, x, i - 1, 0, n - 1);
+            cout << ans + 1 << '\n';
+        }
+    }
+}
+
+signed main() {
+    fasttype;
+    int t = 1;
+    // cin >> t;
+    while (t--) Solve();
+    return 0;
+}
